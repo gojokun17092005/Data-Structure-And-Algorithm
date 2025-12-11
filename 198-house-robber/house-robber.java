41afa36f515514e484a7d84cf5b883641e926085
AYUSH AMAN <144262193+gojokun17092005@users.noreply.github.com>
2025-12-11 15:16:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (31.76%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,19 @@
+class Solution {
+    static Integer[] memo;
+
+    public int rob(int[] nums) {
+        memo = new Integer[nums.length];
+        return robFrom(nums, nums.length - 1, memo);
+    }
+
+    private int robFrom(int[] nums, int i, Integer[] memo) {
+        if (i < 0) return 0;
+        if (memo[i] != null) return memo[i];
+
+        int rob = nums[i] + robFrom(nums, i - 2, memo);
+        int skip = robFrom(nums, i - 1, memo);
+
+        memo[i] = Math.max(rob, skip);
+        return memo[i];
+    }
+}
